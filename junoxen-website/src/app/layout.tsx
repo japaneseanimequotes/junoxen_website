7deef558226f38352b7d94c7faa41ec4c1c0d8ff
japaneseanimequotes <japanese.anime.quotes@gmail.com>
2026-05-06 05:10:01 +0000

    updated layout.tsx

--- junoxen-website/src/app/layout.tsx
+++ junoxen-website/src/app/layout.tsx
@@ -4,14 +4,17 @@ import Footer from "@/components/footer/Footer";
 import FloatingButtons from "@/components/ui/FloatingButtons";
 import { Poppins } from "next/font/google";
 
-// Configure font
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   display: "swap",
 });
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={poppins.className}>
